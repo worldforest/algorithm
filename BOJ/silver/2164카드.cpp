fcd1e01de7072b818cc102a27dc01ecda89eac04
worldforest <worldforest99@gmail.com>
2021-01-06 22:02:03 +0900

    BOJ silver4 queue

---
+++ BOJ/silver/2164카드.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int N, ans;
+    cin >> N;
+
+    queue<int> Q;
+    for(int i = 1; i <= N; i++){
+        Q.push(i);
+    }
+    
+    while(Q.size() != 1){
+        Q.pop();
+        Q.push(Q.front());
+        Q.pop();
+    }
+    cout << Q.front();
+}
